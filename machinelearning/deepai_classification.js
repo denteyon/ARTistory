e43fee4add4b42cf5146553b6d457a1299e2c9c8
paung23 <paung@fandm.edu>
2021-04-17 19:57:50 -0400

    Updated function for deepAI

--- machinelearning/deepai_classification.js
+++ machinelearning/deepai_classification.js
@@ -1,3 +1,7 @@
+const path = require('path');
+const fs = require('fs');
+const directoryPath = path.join(__dirname, 'machinelearning');
+
 const deepai = require('deepai');
 
 deepai.setApiKey('quickstart-QUdJIGlzIGNvbWluZy4uLi4K');
@@ -11,4 +15,28 @@ async function classify(img1, img2) {
     return resp;
 }
 
-exports.classify = classify;
+function runClassification(img) {
+    var results = {};
+
+    var min = 100;
+    var minFile = "";
+
+    fs.readdir(directoryPath, function (err, files) {
+        //handling error
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+
+        files.forEach(function (file) {
+            results[file] = classify(img, file).then(() => 100);
+            if (results[file] < min) {
+                min = results[file];
+                minFile = file;
+            }
+        });
+    });
+
+    return minFile;
+}
+
+exports.runClassification = runClassification;
