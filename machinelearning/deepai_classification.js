1cff1afc3d07e7ee9a1fc2a269951d9f347a37b3
paung23 <paung@fandm.edu>
2021-04-17 18:56:11 -0400

    Made changes

--- machinelearning/deepai_classification.js
+++ machinelearning/deepai_classification.js
@@ -2,10 +2,13 @@ const deepai = require('deepai');
 
 deepai.setApiKey('quickstart-QUdJIGlzIGNvbWluZy4uLi4K');
 
-(async function() {
-    var resp = await deepai.callStandardApi("image-similarity", {
-        image1: "img1.jpeg",
-        image2: "img2.png",
+async function classify(img1, img2) {
+    const resp = await deepai.callStandardApi("image-similarity", {
+        image1: img1,
+        image2: img2,
     });
     console.log(resp);
-})()
+    return resp;
+}
+
+exports.classify = classify;
