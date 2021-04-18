ade33f7713a285717da9aae521a616cafb5b1f8d
paung23 <paung@fandm.edu>
2021-04-17 23:46:34 -0400

    Fixed classify for DeepAI

--- machinelearning/deepai_classification.js
+++ machinelearning/deepai_classification.js
@@ -1,37 +1,29 @@
 const path = require('path');
 const fs = require('fs');
+
 const uploadedPath = path.join(__dirname, 'uploads/');
-const cachePath = path.join(__dirname, 'cache/');
 
 const deepai = require('deepai');
 
-deepai.setApiKey('155a287f-a64d-45cc-a811-7973b39ee8c6');
+deepai.setApiKey('155a287f-a64d-45cc-a811-7973b39ee8c6'); // May need to change
 
 async function classify(checkImg) {
-    let min = 100;
+    var filenames = fs.readdirSync(uploadedPath);
 
-    await fs.readdir(uploadedPath, async function (err, files) {
-        //handling error
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
+    var min = 100;
+
+    for (const file of filenames) {
+        const resp = await deepai.callStandardApi("image-similarity", {
+            image1: fs.createReadStream(uploadedPath + file),
+            image2: fs.createReadStream(checkImg),
+        })
+
+        if (resp.output.distance < min) {
+            min = resp.output.distance;
         }
+    }
 
-        for (const file of files) {
-            const resp = await deepai.callStandardApi("image-similarity", {
-                image1: fs.createReadStream(uploadedPath + file),
-                image2: fs.createReadStream(checkImg),
-            })
-
-            console.log(resp.output.distance);
-
-            if (resp.output.distance < min) {
-                min = resp;
-            }
-        }
-        console.log("Min:" + min.output.distance);
-
-        return min.output.distance;
-    });
+    return min.toString();
 }
 
 exports.classify = classify;
