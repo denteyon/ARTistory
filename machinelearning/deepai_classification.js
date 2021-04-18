e4108e947fd030322c1ce179b1381da8e7168741
paung23 <paung@fandm.edu>
2021-04-17 22:22:36 -0400

    Issues with deepai_classify

--- machinelearning/deepai_classification.js
+++ machinelearning/deepai_classification.js
@@ -1,42 +1,37 @@
 const path = require('path');
 const fs = require('fs');
-const directoryPath = path.join(__dirname, 'machinelearning');
+const uploadedPath = path.join(__dirname, 'uploads/');
+const cachePath = path.join(__dirname, 'cache/');
 
 const deepai = require('deepai');
 
-deepai.setApiKey('quickstart-QUdJIGlzIGNvbWluZy4uLi4K');
+deepai.setApiKey('155a287f-a64d-45cc-a811-7973b39ee8c6');
 
-async function classify(img1, img2) {
-    const resp = await deepai.callStandardApi("image-similarity", {
-        image1: img1,
-        image2: img2,
-    });
-    console.log(resp);
-    return resp;
-}
+async function classify(checkImg) {
+    let min = 100;
 
-function runClassification(img) {
-    var results = {};
-
-    var min = 100;
-    var minFile = "";
-
-    fs.readdir(directoryPath, function (err, files) {
+    await fs.readdir(uploadedPath, async function (err, files) {
         //handling error
         if (err) {
             return console.log('Unable to scan directory: ' + err);
         }
 
-        files.forEach(function (file) {
-            results[file] = classify(img, file).then(() => 100);
-            if (results[file] < min) {
-                min = results[file];
-                minFile = file;
-            }
-        });
-    });
+        for (const file of files) {
+            const resp = await deepai.callStandardApi("image-similarity", {
+                image1: fs.createReadStream(uploadedPath + file),
+                image2: fs.createReadStream(checkImg),
+            })
 
-    return minFile;
+            console.log(resp.output.distance);
+
+            if (resp.output.distance < min) {
+                min = resp;
+            }
+        }
+        console.log("Min:" + min.output.distance);
+
+        return min.output.distance;
+    });
 }
 
-exports.runClassification = runClassification;
+exports.classify = classify;
